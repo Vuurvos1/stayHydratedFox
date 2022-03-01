5fba1d4585c8dd7f3e8037d47540067d5a7fa86a
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-03-01 17:25:34 +0100

    fix: finaly switch to Twitch helix api

--- index.js
+++ index.js
@@ -1,47 +1,21 @@
 require('dotenv').config();
 const tmi = require('tmi.js');
-const api = require('twitch-api-v5');
+const fetch = require('node-fetch');
 
-api.clientID = process.env.TW_CLIENT_ID;
-const usernames = process.env.TW_CHANNELS.split(',');
+const { CLIENT_ID, CLIENT_SECRET, CHANNELS, TW_OAUTH, USERNAME } = process.env;
 
-// Store user ids
-const userIdList = {};
-let userIdArr = [];
+const usernames = CHANNELS.split(',');
+const userQuery = `user_login=${usernames.join('&user_login=')}`;
 
 let liveChannels = [];
 let msgQueue = [];
 const hourMs = 60000 * 60; // ms in a hour
 
-api.users.usersByName(
-  {
-    users: usernames,
-  },
-  (err, res) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(`Found ${res._total} channels`);
-      for (let user of res.users) {
-        userIdList[user.name] = user._id;
-      }
-    }
-
-    if (Object.keys(userIdList).length > usernames.length) {
-      throw new Error('More usernames where fetched than defined in .env');
-    }
-
-    userIdArr = Object.values(userIdList);
-
-    pingStreamUp();
-  }
-);
-
 // Define configuration options
 const opts = {
   identity: {
-    username: process.env.TW_USERNAME,
-    password: process.env.TW_OAUTH,
+    username: USERNAME,
+    password: TW_OAUTH,
   },
   channels: usernames,
 };
@@ -51,7 +25,7 @@ const client = new tmi.client(opts);
 // Register our event handlers (defined below)
 client.on('connected', onConnectedHandler);
 
-// Connect to Twitch:
+// Connect to Twitch
 client.connect();
 
 // Called every time the bot connects to Twitch chat
@@ -62,60 +36,55 @@ function onConnectedHandler(addr, port) {
 // ping all streams
 async function pingStreamUp() {
   try {
-    api.streams.live(
-      {
-        channel: userIdArr.join(),
+    const token = await getAccessToken();
+
+    const url = `https://api.twitch.tv/helix/streams?${userQuery}`;
+    const res = await fetch(url, {
+      headers: {
+        'Client-ID': CLIENT_ID,
+        Authorization: `Bearer ${token.access_token}`,
       },
-      (err, res) => {
-        if (res) {
-          if (!res.streams.length > 0) {
-            return;
-          }
+    });
 
-          liveChannels = [];
+    const json = await res.json();
+    const channels = json.data;
 
-          for (let stream of res.streams) {
-            const streamTime = Math.floor(
-              new Date() - new Date(stream.created_at)
-            );
-            const timeTilReminder = hourMs - (streamTime % hourMs);
-            const hoursLive = Math.ceil(streamTime / hourMs);
-            const streamName = stream.channel.name;
+    if (channels.length < 0) {
+      return; // no channels live
+    }
 
-            liveChannels.push(streamName);
-
-            if (!msgQueue.includes(streamName)) {
-              msgQueue.push(streamName);
-              console.log(
-                `Sending reminder to ${streamName} in ${(
-                  timeTilReminder / 60000
-                ).toFixed(2)} min, ${hoursLive} hour live`
-              );
-
-              setTimeout(() => {
-                sendReminder(streamName, hoursLive);
-              }, timeTilReminder);
-            }
-          }
-        } else {
-          console.log(err);
-        }
+    liveChannels = [];
+    for (let stream of channels) {
+      const streamTime = Math.floor(new Date() - new Date(stream.started_at));
+      const timeTilReminder = hourMs - (streamTime % hourMs);
+      const hoursLive = Math.ceil(streamTime / hourMs);
+      const streamName = stream.user_name;
+      liveChannels.push(streamName);
+      if (!msgQueue.includes(streamName)) {
+        msgQueue.push(streamName);
+        console.log(
+          `Sending reminder to ${streamName} in ${(
+            timeTilReminder / 60000
+          ).toFixed(2)} min, ${hoursLive} hour live`
+        );
+        setTimeout(() => {
+          sendReminder(streamName, hoursLive);
+        }, timeTilReminder);
       }
-    );
+    }
   } catch (error) {
     console.log(error);
   }
 }
 
-function sendReminder(userName, hours) {
+function sendReminder(username, hours) {
   // remove from message queue
-  const x = msgQueue.indexOf(userName);
-  if (x > -1) {
-    // remove user if not in the msgQueue
-    msgQueue.splice(x, 1);
+  const index = msgQueue.indexOf(username);
+  if (index > -1) {
+    msgQueue.splice(index, 1); // remove user if not in the msgQueue
   }
 
-  if (liveChannels.includes(userName)) {
+  if (liveChannels.includes(username)) {
     // calculate hours and water amount
     let water = hours * 120;
 
@@ -123,16 +92,22 @@ function sendReminder(userName, hours) {
     water = water >= 1000 ? `${Math.round(water / 100) / 10} L` : `${water} mL`;
 
     // send message to user
-    console.log(`Send reminder to ${userName} for ${hours} live`);
+    console.log(`Send reminder to ${username} for ${hours} live`);
     client.say(
-      userName,
+      username,
       `You have been live for ${hours} and should have consumed at least ${water} of water to maintain optimal hydration! 💦`
     );
   }
 }
 
-// code loop
-const interval = 5 * 60000; // 5 min
+// main code loop
+pingStreamUp();
 setInterval(() => {
   pingStreamUp();
-}, interval);
+}, 5 * 60000); // 5 minutes
+
+async function getAccessToken() {
+  const url = `https://id.twitch.tv/oauth2/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials`;
+  const res = await fetch(url, { method: 'POST' });
+  return await res.json();
+}
